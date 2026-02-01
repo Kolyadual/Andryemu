00f75d9c3fa5584ca05543843c72bf7f3dfc2cee
Kolyadual <kolyadual@gmail.com>
2026-02-01 20:29:47 +0300

    Update install-on-debian.sh

--- install-on-debian.sh
+++ install-on-debian.sh
@@ -4,3 +4,10 @@
 
 wget https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1769963428/Andryemu-1.2-x86-64.AppImage
 
+sudo apt install libfuse2 fuse
+
+sudo chmod +x Andryemu-1.2-x86_64.AppImage && ./Andryemu-1.2-x86-64.AppImage --appimage-extract
+
+cd squashfs-root && sudo bash systemr.sh
+
+echo "Install complete! Please, run it using command python3 app.py --run"
