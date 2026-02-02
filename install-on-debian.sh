fc718bb2dcb99a606769f26b2df1bdc3913ecd44
Kolyadual <kolyadual@gmail.com>
2026-02-02 10:39:53 +0300

    Update install-on-debian.sh

--- install-on-debian.sh
+++ install-on-debian.sh
@@ -2,18 +2,6 @@
 
 # Andryemu v1.2 install script.
 
-INSTALL_DIR="squashfs-root"
-
-if [ -d "$INSTALL_DIR" ]; then
-    echo -e "${YELLOW} The folder already exists. Overwrite it?${NC}"
-    read -p "[y/N]: " -n 1 -r
-    echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        echo -e "${RED} The installation was canceled.${NC}"
-        exit 1
-    fi
-    rm -rf "$INSTALL_DIR"
-fi
 
 wget https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1769963428/Andryemu-1.2-x86-64.AppImage
 
